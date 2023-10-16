b810ed656be85abf62663713c34ec2f0af5835ef
Steve Vermeulen <sfvermeulen@gmail.com>
2023-10-16 18:38:25 +0800

    Fixed example

--- example/example1.lua
+++ example/example1.lua
@@ -1,5 +1,6 @@
-
+local _tl_compat; if (tonumber((_VERSION or ''):match('[%d.]*$')) or 0) < 5.3 then local p, m = pcall(require, 'compat53.module'); if p then _tl_compat = m end end; local assert = _tl_compat and _tl_compat.assert or assert
 local lusc = require("lusc")
+local uv = require("luv")
 
 local function main()
    print("Waiting 1 second...")
@@ -14,6 +15,7 @@ local function main()
          lusc.await_sleep(1)
          print("Completed child 1")
       end)
+
       nursery:start_soon(function()
          print("Child 2 started.  Waiting 1 second...")
          lusc.await_sleep(1)
@@ -25,4 +27,20 @@ local function main()
    print("Completed all child tasks")
 end
 
-lusc.run(main)
+lusc.start({
+   generate_debug_names = true,
+   on_completed = function(e)
+      if e then
+         print("Lusc failed: " .. tostring(e))
+      end
+   end,
+})
+
+lusc.schedule(main)
+
+
+lusc.stop()
+
+assert(uv.loop_mode() == nil)
+local result, err = uv.run()
+assert(result == false, err)
