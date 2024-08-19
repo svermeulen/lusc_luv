ae2e623c31e6e651a0a1f4576b3407c83d3d61bd
Steve Vermeulen <sfvermeulen@gmail.com>
2024-08-19 22:07:30 +0800

    Changed generate_lua.sh.

--- scripts/generate_lua.sh
+++ scripts/generate_lua.sh
@@ -6,11 +6,11 @@ mkdir ./gen
 mkdir ./gen/lusc
 mkdir ./gen/lusc/internal
 mkdir ./gen/lusc/tests
-tl gen src/lusc/init.tl -o gen/lusc/init.lua
-tl gen src/lusc/internal/util.tl -o gen/lusc/internal/util.lua
+luarocks_tree/bin/tl gen src/lusc/init.tl -o gen/lusc/init.lua
+luarocks_tree/bin/tl gen src/lusc/internal/util.tl -o gen/lusc/internal/util.lua
 cp src/lusc/internal/queue.lua gen/lusc/internal/queue.lua
-tl gen src/lusc/tests/async_helper.tl -o gen/lusc/tests/async_helper.lua
-tl gen src/lusc/tests/lusc_spec.tl -o gen/lusc/tests/lusc_spec.lua
-tl gen src/lusc/tests/setup.tl -o gen/lusc/tests/setup.lua
-tl gen src/lusc/luv_async.tl -o gen/lusc/luv_async.lua
-tl gen src/lusc/tests/luv_async_spec.tl -o gen/lusc/tests/luv_async_spec.lua
+luarocks_tree/bin/tl gen src/lusc/tests/async_helper.tl -o gen/lusc/tests/async_helper.lua
+luarocks_tree/bin/tl gen src/lusc/tests/lusc_spec.tl -o gen/lusc/tests/lusc_spec.lua
+luarocks_tree/bin/tl gen src/lusc/tests/setup.tl -o gen/lusc/tests/setup.lua
+luarocks_tree/bin/tl gen src/lusc/luv_async.tl -o gen/lusc/luv_async.lua
+luarocks_tree/bin/tl gen src/lusc/tests/luv_async_spec.tl -o gen/lusc/tests/luv_async_spec.lua
